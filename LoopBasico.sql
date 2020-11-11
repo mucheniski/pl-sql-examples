a037709aacb40cb33cb2c67b96804ccc5857c426
mucheniski <mucheniski@gmail.com>
2020-11-11 08:57:56 -0300

    27. LOOP básico

---
+++ LoopBasico.sql
@@ -0,0 +1,13 @@
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(38) := 1;
+  vLimite  NUMBER(38) := &pLimite;
+BEGIN
+-- Imprimindo números de 1 até o limite
+    LOOP 
+      DBMS_OUTPUT.PUT_LINE('Número = ' || to_char(vNumero));
+      EXIT WHEN vNumero = vLimite;
+      vNumero := vNumero + 1;
+    END LOOP;
+END;
